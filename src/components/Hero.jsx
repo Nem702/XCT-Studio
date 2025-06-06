af2fe1872885f9444dfcb0b322f8e0ff4a3fc78d
Nemanja Sesum <nemanja94sesa@gmail.com>
2025-06-05 20:25:18 -0700

    gsap clips id, z-index positions, scroll text animation for Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,44 +1,46 @@
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
-gsap.registerPlugin(ScrollTrigger)
+import { useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
 
-useGSAP(() => {
-  gsap.fromTo("#bg-video", {
-    clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
-  },
-  {
-    clipPath: 'polygon(0 0, 100% 0, 83% 93%, 12% 88%)',
-    scrollTrigger: {
-      trigger: '#bg-video',
-      start: 'center center',
-      end: 'bottom center',
-      scrub: true,
-    }
-  })
-})
-
+  useEffect(() => {
+    gsap.fromTo("#bg-video", {
+      clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
+    },
+    {
+      clipPath: 'polygon(0 0, 100% 0, 83% 93%, 12% 88%)',
+      scrollTrigger: {
+        trigger: '#bg-video',
+        start: 'center center',
+        end: 'bottom center',
+        scrub: true,
+      }
+    });
+  }, []);
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
-        <div className='h-full w-full'>
-          <video
-              id='bg-video'
-              autoPlay
-              muted
-              loop
-              className='h-full w-full object-cover'
-          >
-              <source src='assets/Aurora7s.mp4' type='video/mp4' />
-          </video>
+      {/* gsap clips id, z-index positions, scroll text animation */}
+      <div id='bg-video' className='h-full w-full relative z-40'>
+        <video
+          autoPlay
+          muted
+          loop
+          className='h-full w-full object-cover z-0'
+        >
+          <source src='assets/Aurora7s.mp4' type='video/mp4' />
+        </video>
+        <div className='absolute inset-0 flex items-end justify-end p-5'>
+          <h1 className='text-blue-200 text-7xl font-bold z-50'>XCT Studio</h1>
         </div>
-        <div>
-          <h1 className='absolute bottom-5 right-5 z-40 text-blue-200 text-7xl font-bold'>XCT Studio</h1>
-        </div>
+      </div>
+      <div className='absolute inset-0 flex items-end justify-end p-5 z-0'>
+        <h1 className='text-black text-7xl font-bold'>XCT Studio</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
